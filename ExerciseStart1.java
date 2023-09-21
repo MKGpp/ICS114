59854e014396c3655916ce7b4ce53b8207f43650
MKG <mattgolshani@gmail.com>
2023-09-21 10:40:15 -0700

    added an excercise file to work on

---
+++ ExerciseStart1.java
@@ -0,0 +1,96 @@
+/*
+Goals: To gain experience with
+1. declaring and initializing variables
+2. arithmetic operators: / %
+3. type casting (explicit and implicit)
+4. syntax errors
+5. constants
+
+Instructions:
+Provide the Java code indicated by the comments (//)
+Save, compile and run after each step.
+When there is a question, supply your answer in a comment.
+(This will be useful to you when you are studying later.)
+Do not remove the comments once you have completed a task!!
+*/
+
+public class ExerciseS {
+    public static void main(String args[]) {
+        System.out.println("Variables, Arithmetic Operators, and Type Casting\n");
+        
+        // 1. Here: Declare 2 integer variables with variable names: num, count.
+        //       Initialize both to zero.
+        //       Question: What if you initialize an integer to 0.0? Try it.
+        
+        // 2. Here: Declare 2 double variables with variables names: temperature, price.
+        //       Initialize both to zero.
+        //       Question: Do you have to use 0.0 (ie Can you just use 0?) Why or why not?
+        
+        // 3. Here: Remove the comment indicator from the following output statement.
+        //       Observe the \n. What does it do?
+        // System.out.println("Temperature is: " + temperature + "\nPrice is: " + price);
+        
+        // 4. Here: Declare a float variable called depth.
+        //       Initialize it to 0.0.
+        //       Compile. What happens? Why? How can we correct the error? (four ways)
+        
+        // 5. Here: Provide an assignment statement to store 150.99 in variable price.
+        
+        // 6. Here: Provide an assignment statement to store price in count.
+        //      What happens? Why? How can it be fixed?
+        
+        // 7. Here: Remove the comment from the following statement to display price and count.
+        //       Think: What do you expect to happen?
+        // System.out.println("price is " + price + " and count is " + count);
+        
+        // 8. Here: Provide an assignment statement to store the value 10 in variable num.
+        
+        // 9. Here: Provide an assignment statement to store the value 3 in variable count.
+        
+        // 10. Here: Declare an integer variable called quotient.
+        //       Initialize it to num/count
+        
+        // 11. Here: Remove the comment indicator from the following output statement.
+        // System.out.println(num + "/" + count + ": quotient is: " + quotient);
+        
+        // 12. Here: Declare an integer variable called remainder.
+        //       Initialize it to the remainder when num is divided by count.
+        
+        // 13. Here: Copy the System.out.println statement from above and paste it here.
+        //       Change quotient to remainder (two occurrences.)
+        
+        // 14. Here: Declare a float variable called floatQuotient.
+        //       Initialize it to num / count.
+        
+        // 15. Here: Again, paste the System.out.println statement here.
+        //       This time, display floatQuotient to the screen.
+        
+        // 16. Here: Change all of the above System.out.println statements to System.out.print statements.
+        //       Observe the output. What happens? Why?
+        
+        // 17. Here: Declare a float called value.
+        //       Initialize it to num / count.
+        //       Notice the implicit type conversion that is done.
+		//		 Again, paste the System.out.println statement here and make the appropriate changes.
+        
+        // 18. Here: Copy and paste the above two lines and then change the integer division operator to the modulo operator.
+        //       You will have to make one change. Try to compile to determine what needs to be changed.
+        //       Is there implicit type conversion again?
+		//	     Yet, again, provide a System.out.println statement to see what is happening.
+		
+		// 19. Here: Let's see floating point division.
+		//       Declare variables called size, height, width. The first two are of type float and the last of type int.
+		//       Initialize height to 100.3 and width to 7.
+		//       Provide an assignment statement that assigns size the value of height / width.
+		// 	     Question: What type of division is this?
+		//       Write a System.out.println statement to output size, height and width.
+        
+        // 20. Here: This is the formula to convert a Fahrenheit temperature to Celcius.
+        //     5 / 9 * (fahrenheit -32)
+        //     Declare and initialize variables to perform this conversion.
+        //     You need not get input from the user. Simply use an assignment statement to store a value in fahrenheit.
+        //     Output the celsius value as a real number.
+        //     Once you have done this, replace the values 5, 9, 32 with constants. (See zyBooks: Constants)
+		
+    }
+}
