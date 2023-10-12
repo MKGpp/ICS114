ce185e09d8cbe7ffb23e62af9ed278a2708420de
Matthew <100318133+MKGpp@users.noreply.github.com>
2023-10-11 19:49:35 -0700

    Add files via upload
    added an exercise in pseudo code witing

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def main():
+    
+    sum = 0
+    max = int(input("Please enter max number: "))
+    
+    for i in range(1, max + 1):
+        sum += i
+
+    print(sum)
+
+
+
+if __name__ == "__main__":
+    main()
+    
